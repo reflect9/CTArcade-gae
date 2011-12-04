85ec3f1367d5a174333710b107290e8b4d9c74b7
Matthew Mauriello <mattm401@umd.edu>
2011-12-04 10:50:36 -0500

    Updated cron jobs to run once per day (TESTING) Updated task queues Add favicon Updated navigation flow (start on intro) Updated Intro page (with info) Updated lobby welcome Updated various error catches in main in playMatch and Trainer Added <title> to all pages

--- src/main.py
+++ src/main.py
@@ -148,8 +148,11 @@ class PlayMatch(webapp.RequestHandler):
         try:
             userID = session['loggedInAs']
         except KeyError:
-            userID = "Guest"   
-        opponent = self.request.get("p2") 
+            userID = "Guest"
+	if userID != self.request.get("p2"):   
+            opponent = self.request.get("p2")
+        else:
+            opponent = "" 
         print >>sys.stderr, userID
         template_values = {
             'userID' : userID,
@@ -161,15 +164,14 @@ class PlayMatch(webapp.RequestHandler):
        
 class Trainer(webapp.RequestHandler):
     def get(self):
-#        try:
-        session = sessions.Session()
-        if session["loggedInAs"]:
-            current_user_id = session["loggedInAs"]
-        else:
-            self.redirect('/')
-#        current_user_id = self.request.get('id')
-#        except AttributeError:
-#            current_user_id='ben'
+        try:
+            session = sessions.Session()
+            if session["loggedInAs"]:
+                current_user_id = session["loggedInAs"]
+            else:
+                self.redirect('/')
+        except:
+            current_user_id='Guest'
         template_values = {
             'user_id': current_user_id,
         }  
@@ -259,67 +261,80 @@ class ReviewMatch(webapp.RequestHandler):
 
 class CounterWorker(webapp.RequestHandler):
     def post(self): # should run at most 1/s
-        log = TournamentLog(message = "Tournament Start")
+        #log = TournamentLog(message = "Tournament Start")
         print >>sys.stderr, "Tournament Start"
-        log.put()
+        #log.put()
         users = User.all()
         users.order('-score')
         tournament_entries = []
         for p1 in users:
                 tournament_entries.append(str(p1.id))
-        log = TournamentLog(message = str(("Loaded:", len(tournament_entries))))
-        log.put()
+        #log = TournamentLog(message = str(("Loaded:", len(tournament_entries))))
+        #log.put()
         print >>sys.stderr, str(("Loaded:", len(tournament_entries)))
-        taskqueue.add(url='/round', params={'tournament_entries': json.dumps(tournament_entries)}, countdown=60)
+        taskqueue.add(url='/round', params={'tournament_entries': json.dumps(tournament_entries)}, countdown=200)
     def get(self): # should run at most 1/s
-        log = TournamentLog(message = "Tournament Start")
+        #log = TournamentLog(message = "Tournament Start")
         print >>sys.stderr, "Tournament Start"
-        log.put()
+        #log.put()
         users = User.all()
         users.order('-score')
         tournament_entries = []
         for p1 in users:
                 tournament_entries.append(str(p1.id))
-        log = TournamentLog(message = str(("Loaded:", len(tournament_entries))))
-        log.put()
+        #log = TournamentLog(message = str(("Loaded:", len(tournament_entries))))
+        #log.put()
         print >>sys.stderr, str(("Loaded:", len(tournament_entries)))
-        taskqueue.add(url='/round', params={'tournament_entries': json.dumps(tournament_entries)}, countdown=60)
+        taskqueue.add(url='/round', params={'tournament_entries': json.dumps(tournament_entries)}, countdown=200)
+		
         
 class RoundWorker(webapp.RequestHandler):
     def post(self): # should run at most 1/s
-        Message = "%s%s" % ("Round Start: ", self.request.get('tournament_entries'))
-        print >>sys.stderr, Message
-        log = TournamentLog(message = Message)
-        log.put()
+        try:
+       	    Message = "%s%s" % ("Round Start: ", self.request.get('tournament_entries'))
+            print >>sys.stderr, Message
+            #log = TournamentLog(message = Message)
+            #log.put()
             
-        round_entries = []
-        print >>sys.stderr, "Round Entries Created"
-        round_winners = []
-        print >>sys.stderr, "Round Winners Created"
-        round_entries = json.loads(self.request.get('tournament_entries'))
-        print >>sys.stderr, "Round Entries Loaded"
-        while len(round_entries) >= 2:
-            player1 = round_entries.pop()
-            player2 = round_entries.pop()
-            match = TicTacToeMatch(player1,player2,game='tictactoe',turn=player1)
-            result = match.run()
-            while result["winner"] == "Tie Game":
+            round_entries = []
+            print >>sys.stderr, "Round Entries Created"
+            round_winners = []
+            print >>sys.stderr, "Round Winners Created"
+            round_entries = json.loads(self.request.get('tournament_entries'))
+            print >>sys.stderr, "Round Entries Loaded"
+            while len(round_entries) >= 2:
+                player1 = round_entries.pop()
+                print >>sys.stderr,"Pop"
+                player2 = round_entries.pop()
+                print >>sys.stderr,"Pop"
+                match = TicTacToeMatch(player1,player2,game='tictactoe',turn=player1)
                 result = match.run()
-            round_winners.append(str(result["winner"]))
-            Message = "%s%s" % ("Round Winner: ", str(result["winner"]))
-            print >>sys.stderr, Message
-            log = TournamentLog(message = Message)
-            log.put()
-        # Need to do something about adding basic scores with person who gets the by
-        by = "None"                
-        if len(round_entries) == 1:
-            by = round_entries.pop()
-            Message = "%s%s" % ("Assigning By:", str(by))            
-            print >>sys.stderr, Message
-            log = TournamentLog(message = Message)
-            log.put()
-        # Determine if another round is necessary or if winner can be declared    
-        taskqueue.add(url='/score', params={'tournament_entries': json.dumps(round_winners), 'by': by}, countdown=60)
+                print >>sys.stderr, "Match"
+                while result["winner"] == "Tie Game":
+                    result = match.run()
+                    print >>sys.stderr, "Tie"
+                round_winners.append(str(result["winner"]))
+                #Message = "%s%s" % ("Round Winner: ", str(result["winner"]))
+                print >>sys.stderr, result["winner"]
+                #log = TournamentLog(message = Message)
+                #log.put()
+            # Need to do something about adding basic scores with person who gets the by
+            print >>sys.stderr, "Prepping Exit"
+            by = "None"                
+            if len(round_entries) == 1:
+                by = round_entries.pop()
+                Message = "%s%s" % ("Assigning By:", str(by))            
+                print >>sys.stderr, Message
+                #log = TournamentLog(message = Message)
+                #log.put()
+            # Determine if another round is necessary or if winner can be declared
+            print >>sys.stderr, "Exiting"    
+            taskqueue.add(url='/score', params={'tournament_entries': json.dumps(round_winners), 'by': by}, countdown=200)
+
+        except:
+            self.response.clear()
+            self.response.set_status(500)
+            print >>sys.stderr, "Round Interrupted"
 
 class ScoreWorker(webapp.RequestHandler):
     def post(self): # should run at most 1/s
@@ -329,8 +344,8 @@ class ScoreWorker(webapp.RequestHandler):
         if len(score_entries) == 1 and self.request.get('by') == "None":
             Message = "%s%s" % ("Tournament Winner:", str(score_entries))    
             print >>sys.stderr, Message
-            log = TournamentLog(message = Message)
-            log.put()
+            #log = TournamentLog(message = Message)
+            #log.put()
             tournament_winner = score_entries.pop()
             updateUser = db.GqlQuery("SELECT * FROM User WHERE id=:1",tournament_winner).get()
             updateUser.score += 10
@@ -344,9 +359,9 @@ class ScoreWorker(webapp.RequestHandler):
             if self.request.get('by') != "None":
                 next_round.append(self.request.get('by'))
             if len(next_round) >= 2:     
-                taskqueue.add(url='/round', params={'tournament_entries': json.dumps(next_round)}, countdown=60)
+                taskqueue.add(url='/round', params={'tournament_entries': json.dumps(next_round)}, countdown=200)
 def main():
-    application = webapp.WSGIApplication([('/', Lobby),
+    application = webapp.WSGIApplication([('/', Intro),
                                           ('/init', Init),
                                           ('/signUp',SignUp),
                                           ('/LogIn',LogIn),
@@ -359,8 +374,8 @@ def main():
                                           ('/reviewMatch',ReviewMatch), 
                                           ('/worker', CounterWorker), 
                                           ('/round', RoundWorker),
-                                          ('/score', ScoreWorker),
-										  ('/intro', Intro),                               
+                                          ('/score', ScoreWorker),								  ('/lobby', Lobby),
+  
                                           ],
                                          debug=True)
     util.run_wsgi_app(application)
