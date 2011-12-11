0b8de4718f93671e314f203fe79f484fa278b1d3
Matthew Mauriello <mattm401@umd.edu>
2011-12-11 02:47:21 -0500

    Fixed Midnight bug in lobby timer Fixed Tournament bug for tie games (although Madeyjay's rule is still causing a failure)

--- src/main.py
+++ src/main.py
@@ -306,7 +306,8 @@ class CounterWorker(webapp.RequestHandler):
         
 class RoundWorker(webapp.RequestHandler):
     def post(self): # should run at most 1/s
-        try:
+       # try:
+       	    tie_counter = 0;
        	    Message = "%s%s" % ("Round Start: ", self.request.get('tournament_entries'))
             print >>sys.stderr, Message
             #log = TournamentLog(message = Message)
@@ -325,12 +326,16 @@ class RoundWorker(webapp.RequestHandler):
                 print >>sys.stderr,"Pop"
                 p1_AI = getUserRuleDict(player1, 'tictactoe')
                 p2_AI = getUserRuleDict(player2, 'tictactoe')
+                print >>sys.stderr,"AIs Loaded"
                 result = TicTacToe.runMatch(player1,player2,player1,p1_AI,p2_AI)
-                print >>sys.stderr, "Match"
-                while result["winner"] == "Tie Game":
+                while result["winner"] == "Tie Game" and tie_counter < 20:
                     result = TicTacToe.runMatch(player1,player2,player1,p1_AI,p2_AI)
                     print >>sys.stderr, "Tie"
-                round_winners.append(str(result["winner"]))
+                    tie_counter = tie_counter + 1
+                if result["winner"] == "Tie Game":
+                    round_winners.append(player1)
+                else:	
+                    round_winners.append(str(result["winner"]))
                 #Message = "%s%s" % ("Round Winner: ", str(result["winner"]))
                 print >>sys.stderr, result["winner"]
                 #log = TournamentLog(message = Message)
@@ -348,10 +353,10 @@ class RoundWorker(webapp.RequestHandler):
             print >>sys.stderr, "Exiting"    
             taskqueue.add(url='/score', params={'tournament_entries': json.dumps(round_winners), 'by': by}, countdown=180)
 
-        except:
-            self.response.clear()
-            self.response.set_status(500)
-            print >>sys.stderr, "Round Interrupted"
+       # except:
+        #    self.response.clear()
+         #   self.response.set_status(500)
+          #  print >>sys.stderr, "Round Interrupted"
 
 class ScoreWorker(webapp.RequestHandler):
     def post(self): # should run at most 1/s
